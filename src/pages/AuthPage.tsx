9a6e03cdb71c9f89ef1dbc5d1217c11249f261bb
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-25 16:02:28 +0900

    feat: 페이지 라우팅 세팅

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthContainer from '../components/AuthContainer';
+
+const AuthPage: React.FC = () => {
+  return <AuthContainer />;
+};
+
+export default AuthPage;
