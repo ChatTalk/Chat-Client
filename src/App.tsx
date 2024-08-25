7d0873cd366c16c66e06aaa42c75b9fc1a41d1fa
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-25 16:34:09 +0900

    chore: 의존성 axios 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
-import { GlobalStyle } from './styles/GlobalStyles';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/" element={<AuthPage />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/pages/AuthPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuthContainer from '../components/AuthContainer';
-
-const AuthPage: React.FC = () => {
-  return <AuthContainer />;
-};
-
-export default AuthPage;
