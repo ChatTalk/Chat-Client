5fc214bca2f8c5973e2b8844c64b6cfdb96c92f2
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-29 22:17:02 +0900

    refactor: 메세지 발신자 정보 UI 표시

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -10,6 +10,7 @@ const ChatMessage: React.FC<ChatMessageCardProps> = ({ message }) => {
 
     return (
         <MessageCardContainer>  
+            <h4>{message.username}</h4>
             <p>{message.message}</p>
             <Timestamp>{new Date(message.createdAt).toLocaleTimeString()}</Timestamp>
         </MessageCardContainer>
