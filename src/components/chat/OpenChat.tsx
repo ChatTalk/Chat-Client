00f0fee7b7299e2e60e768ddf657502a5d31404a
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-29 18:07:30 +0900

    feat: 채팅 id 할당 및 라우팅용 처리

--- src/components/chat/OpenChat.tsx
+++ src/components/chat/OpenChat.tsx
@@ -1,75 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import { ChatCreateButtonContainer, OpenChatListContainer } from '../../styles/ChatListStyles';
+import { useNavigate } from 'react-router-dom';
+import { ChatCreateButtonContainer, OpenChatListContainer, ChatCardContainer, StyledButton } from '../../styles/ChatListStyles';
 import { createOpenChat, getAllOpenChats } from '../../api/ChatApi';
 
 interface ChatInfo {
+    chatId: string;
     title: string;
     openUsername: string;
     maxPersonnel: number;
-  }
+}
   
-  const OpenChat: React.FC = () => {
-      const [chats, setChats] = useState<ChatInfo[]>([]);
-  
-      useEffect(() => {
-          const fetchChats = async () => {
-              try {
-                  const response = await getAllOpenChats();
-                  setChats(response.data);
-              } catch (error) {
-                  console.error('Error fetching chats:', error);
-              }
-          };
-  
-          fetchChats();
-      }, []);
-  
-      const handleCreateChat = async () => {
-          try {
+const OpenChat: React.FC = () => {
+    const [chats, setChats] = useState<ChatInfo[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchChats = async () => {
+            try {
+                const response = await getAllOpenChats();
+                const chats: ChatInfo[] = response.data;
+                setChats(chats);
+            } catch (error) {
+                console.error('Error fetching chats:', error);
+            }
+        };
+
+        fetchChats();
+    }, []);
+
+    const handleCreateChat = async () => {
+        try {
             const title = window.prompt('채팅방 제목을 입력하세요');
             if (!title) {
-              alert('채팅방 제목을 입력해야 합니다.');
-              return;
+                alert('채팅방 제목을 입력해야 합니다.');
+                return;
             }
-            
+
             const maxPersonnelString = window.prompt('제한 인원을 입력하세요');
             if (maxPersonnelString === null) {
-              alert('제한 인원을 입력해야 합니다.');
-              return;
+                alert('제한 인원을 입력해야 합니다.');
+                return;
             }
-            
+
             const maxPersonnel = parseInt(maxPersonnelString, 10);
             if (isNaN(maxPersonnel)) {
-              alert('유효한 숫자를 입력해야 합니다.');
-              return;
+                alert('유효한 숫자를 입력해야 합니다.');
+                return;
             }
-  
+
             if (title && maxPersonnel) {
                 await createOpenChat({ title, maxPersonnel });
                 alert('채팅방이 생성됐습니다.');
             }
-          } catch (error) {
-              console.error('Error creating chat:', error);
-              alert('채팅방 생성 에러');
-          }
-      };
-  
-      return (
-          <>
-              <OpenChatListContainer>
-                  {chats.map((chat, index) => (
-                      <div key={index}>
-                          <h4>{chat.title}</h4>
-                          <p>Host: {chat.openUsername}</p>
-                          <p>Max Personnel: {chat.maxPersonnel}</p>
-                      </div>
-                  ))}
-              </OpenChatListContainer>
-              <ChatCreateButtonContainer>
-                  <button onClick={handleCreateChat}>Create Chat Room</button>
-              </ChatCreateButtonContainer>
-          </>
-      );
-  };
-  
-  export default OpenChat;
+        } catch (error) {
+            console.error('Error creating chat:', error);
+            alert('채팅방 생성 에러');
+        }
+    };
+
+    const handleChatClick = (chatId: string) => {
+        navigate(`/chat/${chatId}`);
+    };
+
+    return (
+        <>
+            <OpenChatListContainer>
+                {chats.map((chat) => (
+                    <ChatCardContainer key={chat.chatId} onClick={() => handleChatClick(chat.chatId)}>
+                        <h4>{chat.title}</h4>
+                        <p>Host: {chat.openUsername}</p>
+                        <p>Max Personnel: {chat.maxPersonnel}</p>
+                    </ChatCardContainer>
+                ))}
+            </OpenChatListContainer>
+            <ChatCreateButtonContainer>
+                <StyledButton onClick={handleCreateChat}>Create Chat Room</StyledButton>
+            </ChatCreateButtonContainer>
+        </>
+    );
+};
+
+export default OpenChat;
