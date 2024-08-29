9866cb94fa3d7555e04fc7bfeb6fcc2045f9a6bf
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-29 22:14:37 +0900

    refactor: 웹소켓 로직 구현을 위한 더미데이터 분리

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,40 +1,10 @@
 import React from "react";
 import { ChatInputContainer, ChatMessagesContainer } from "../../styles/ChatStyles"
 import ChatMessageList from "./ChatMessageList";
-import { ChatMessageDTO } from "../../styles/MessageTypes";
+import { dummyMessages } from "../../util/Dummys";
 
 const Chat: React.FC = () => {
-    const dummyMessages: ChatMessageDTO[] = [
-        {
-            chatId: "1",
-            type: "ENTER",
-            username: "User1",
-            message: "User1 님이 입장하셨습니다.",
-            createdAt: new Date().toISOString(),
-        },
-        {
-            chatId: "1",
-            type: "MESSAGE",
-            username: "User2",
-            message: "안녕하세요!",
-            createdAt: new Date().toISOString(),
-        },
-        {
-            chatId: "1",
-            type: "LEAVE",
-            username: "User3",
-            message: "User3 님이 퇴장하셨습니다.",
-            createdAt: new Date().toISOString(),
-        },
-        {
-            chatId: "1",
-            type: "LEAVE",
-            username: "User3",
-            message: "User3 님이 퇴장하셨습니다.",
-            createdAt: new Date().toISOString(),
-        },
-    ];
-
+    
     return (
         <>
             <ChatMessagesContainer>
