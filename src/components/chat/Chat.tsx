0c26b8ee1ef61622015cdd76dd210e86017aed4b
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-29 22:04:58 +0900

    feat: 메세지 카드 컴포넌트 더미데이터 생성 및 UI 확인

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChatInputContainer, ChatMessagesContainer } from "../../styles/ChatStyles"
+import ChatMessageList from "./ChatMessageList";
+import { ChatMessageDTO } from "../../styles/MessageTypes";
 
 const Chat: React.FC = () => {
+    const dummyMessages: ChatMessageDTO[] = [
+        {
+            chatId: "1",
+            type: "ENTER",
+            username: "User1",
+            message: "User1 님이 입장하셨습니다.",
+            createdAt: new Date().toISOString(),
+        },
+        {
+            chatId: "1",
+            type: "MESSAGE",
+            username: "User2",
+            message: "안녕하세요!",
+            createdAt: new Date().toISOString(),
+        },
+        {
+            chatId: "1",
+            type: "LEAVE",
+            username: "User3",
+            message: "User3 님이 퇴장하셨습니다.",
+            createdAt: new Date().toISOString(),
+        },
+        {
+            chatId: "1",
+            type: "LEAVE",
+            username: "User3",
+            message: "User3 님이 퇴장하셨습니다.",
+            createdAt: new Date().toISOString(),
+        },
+    ];
+
+    // const [messages, setMessages] = useState<ChatMessageDTO[]>(dummyMessages);
+
     return (
         <>
             <ChatMessagesContainer>
             {/* 여기에 메시지 리스트를 표시 */}
+                <ChatMessageList messages={dummyMessages} />
             </ChatMessagesContainer>
             <ChatInputContainer>
                 <input type="text" placeholder="Type your message..." />
--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -9,7 +9,7 @@ interface ChatMessageCardProps {
 const ChatMessage: React.FC<ChatMessageCardProps> = ({ message }) => {
 
     return (
-        <MessageCardContainer>
+        <MessageCardContainer>  
             <p>{message.message}</p>
             <Timestamp>{new Date(message.createdAt).toLocaleTimeString()}</Timestamp>
         </MessageCardContainer>
